aaeee5931f35ec9e0eee8a072c145801b4f131a4
Samour <aaron.burke.cpp@gmail.com>
2022-02-12 14:23:16 +1100

    Fix lint errors

--- fe/src/views/LandingView/index.tsx
+++ fe/src/views/LandingView/index.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react';
+import {useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 const LandingView = (): JSX.Element | null => {
   const navigate = useNavigate();
   // Only login page is currently implemented
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => navigate('/login'), []);
 
   return null;
