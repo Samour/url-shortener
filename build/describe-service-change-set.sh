c573b74255115f494c153301c96e91e3aa59d2c7
Samour <aaron.burke.cpp@gmail.com>
2022-02-15 01:15:34 +1100

    Update describe-service-change-set to use polling instead of sleep

--- build/describe-service-change-set.sh
+++ build/describe-service-change-set.sh
@@ -5,13 +5,37 @@ cd "${0%/*}"
 
 . ./VARS
 
-# Easier than polling AWS until changeset is ready
-sleep 15
-
 CS_VERSION=$(echo -n "$VERSION" | tr '_' '-')
 CHANGE_SET_NAME="${SERVICE_NAME}-app-${CS_VERSION}"
 
-aws cloudformation --output table \
-  describe-change-set \
-  --change-set-name $CHANGE_SET_NAME \
-  --stack-name ${SERVICE_NAME}-app
+cs-report() {
+  aws cloudformation --output $1 \
+    describe-change-set \
+    --change-set-name $CHANGE_SET_NAME \
+    --stack-name ${SERVICE_NAME}-ecr
+}
+
+cs-status() {
+  cs-report json | jq ".Status"
+}
+
+STACK_STATUS=`cs-status`
+WAIT_COUNT=0
+while [ "$STACK_STATUS" = '"CREATE_IN_PROGRESS"' ]; do
+  if [ $WAIT_COUNT -gt 6 ]; then
+    echo "Wait timeout" 1>&2
+    exit 1
+  fi
+
+  echo "Change set in $STACK_STATUS; waiting"
+  sleep 10
+  STACK_STATUS=`cs-status`
+  WAIT_COUNT=$((WAIT_COUNT + 1))
+done
+
+if [ "$STACK_STATUS" = '"CREATE_COMPLETE"' ]; then
+  cs-report table
+else
+  echo "Change set in $STACK_STATUS" 1>&2
+  exit 1
+fi
