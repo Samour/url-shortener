6777d8b727aa1bcf114e43c720ab89adedea3fc5
Samour <aaron.burke.cpp@gmail.com>
2022-02-14 23:08:42 +1100

    Add step to review plan

---
+++ build/describe-service-change-set.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+cd "${0%/*}"
+
+. ./VARS
+
+CS_VERSION=$(echo -n "$VERSION" | tr '_' '-')
+CHANGE_SET_NAME="${SERVICE_NAME}-app-${CS_VERSION}"
+
+aws cloudformation --output table \
+  describe-change-set \
+  --change-set-name $CHANGE_SET_NAME \
+  --stack-name ${SERVICE_NAME}-app
